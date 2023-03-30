0f490c0fb847256539b8ef3eb1a4dc30ba601457
Gruzin <gruzd.roman@inbox.ru>
2023-03-30 15:30:13 +0300

    commit

--- tasks/task.py
+++ tasks/task.py
@@ -5,4 +5,15 @@ def seq_sum(sequence: Union[List, Tuple]) -> int:
     """
     Add your code here or call it from here   
     """
-    pass
+    sum_numbers = 0
+    for i in sequence:
+        try:
+            sum_numbers +=i
+        except TypeError:
+            sum_numbers += seq_sum(i)
+    return sum_numbers
+
+sequence = [1, 2, 3, [4, 5, (6, 7)]]
+
+print(seq_sum(sequence))
+
